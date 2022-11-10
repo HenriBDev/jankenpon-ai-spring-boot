838cd9785a96be375e3a9951a5c4a8d5a6241bf3
HenriBDev <henribdev@gmail.com.br>
2022-11-09 23:26:20 -0300

    Inicializado projeto com Spring Boot

---
+++ src/main/java/jankenpo_a3/jankenpo_a3/JankenpoA3Application.java
@@ -0,0 +1,13 @@
+package jankenpo_a3.jankenpo_a3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankenpoA3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankenpoA3Application.class, args);
+	}
+
+}
---
+++ src/test/java/jankenpo_a3/jankenpo_a3/JankenpoA3ApplicationTests.java
@@ -0,0 +1,13 @@
+package jankenpo_a3.jankenpo_a3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankenpoA3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
