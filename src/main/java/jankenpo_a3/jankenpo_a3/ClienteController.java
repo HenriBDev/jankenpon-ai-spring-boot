97424660534a026d74366e414a15d9535e0f73e7
HenriBDev <henribdev@gmail.com.br>
2022-11-30 23:22:28 -0300

    Conexao com Server estabelecida

--- src/main/java/jankenpo_a3/jankenpo_a3/ClienteController.java
+++ src/main/java/jankenpo_a3/jankenpo_a3/ClienteController.java
@@ -10,28 +10,22 @@ import java.net.ServerSocket;
 @RestController
 public class ClienteController {
 
-    private String HOST_SERVER = "localhost";
+    private String IP_SERVER = "localhost";
     private int PORTA_SERVER = 8081;
-    private int PORTA_CLIENTE = 8000;
-    private String HOST_CLIENTE = "localhost";
     private Socket socketCliente;
-    private Socket socketResposta;
-    private ServerSocket socketServer;
     private DataOutputStream serverOutput;
     private DataInputStream respostaInput;
+    private String resposta;
 
     @PostMapping("/conectarJogo")
     public String conectarJogo(){
         try{
-            this.socketCliente = new Socket("localhost", PORTA_SERVER);
+            this.socketCliente = new Socket(IP_SERVER, PORTA_SERVER);
             this.serverOutput = new DataOutputStream(this.socketCliente.getOutputStream());
             this.serverOutput.writeUTF("Conexão");
             this.serverOutput.flush();
-            this.socketServer = new ServerSocket(PORTA_CLIENTE);
-            this.socketResposta = this.socketServer.accept();
-            this.respostaInput = new DataInputStream(this.socketResposta.getInputStream());
-            String resposta = this.respostaInput.readUTF();
-            this.socketResposta.close();
+            this.respostaInput = new DataInputStream(this.socketCliente.getInputStream());
+            this.resposta = this.respostaInput.readUTF();
             return resposta;
         }
         catch(Exception e){
